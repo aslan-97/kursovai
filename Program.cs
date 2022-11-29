ae863455ab2b7befa452db132a06eb1269f5ea47
aslan <aslanchik19977777@gmail.com>
2022-11-29 22:43:08 +0300

    The end)

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿string[] array = {"1, 12, abcd, =), 12345, 674"};
+var massiv = new string[array.Length];
+int j = 0;
+{
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i].Length < 4)
+    massiv[j] = array[i];
+    j++;
+}
+Console.WriteLine($"[{string.Join(", ", massiv[j])}]");
+//Console.WriteLine($"значение менее 3 элементов: {massiv[j]}");
+}
+
+
+
+
+
+
